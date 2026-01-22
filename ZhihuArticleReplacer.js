5b20074f1afe68655d6a475ad8ecca95de202b28
CookieYEU <102860628+CookieYEU@users.noreply.github.com>
2026-01-22 23:32:06 +0800

    Refactor image replacement logic in ZhihuArticleReplacer
    Updated image size and improved performance by checking for target text before replacement.

--- ZhihuArticleReplacer.js
+++ ZhihuArticleReplacer.js
@@ -1,16 +1,22 @@
-// 获取原始的响应体（JSON字符串）
+// 获取响应体
 let body = $response.body;
 
-// 定义我们要替换的目标和新图片
-// 注意：知乎正文通常是 HTML 格式，所以我们直接插入 img 标签
-const oldText = /\[惊喜\]/g;
-const newImage = '<img src="https://pic2.zhimg.com/v2-3846906ea3ded1fabbf1a98c891527fb.png" style="width: 20px; vertical-align: middle;">';
+// 定义要查找的文字 (正则匹配 [惊喜])
+// 注意：如果还有其他表情想换，可以继续加
+const targetText = /\[惊喜\]/g;
 
-// 执行替换
-// 只有当 body 里真的有这个词时才替换，节省性能
+// 定义要替换成的图片 HTML 代码
+// 这里用了你之前指定的那张图，设置宽度为 20px 以匹配文字大小
+const newImage = '<img src="https://pic2.zhimg.com/v2-3846906ea3ded1fabbf1a98c891527fb.png" style="width: 22px; vertical-align: middle; display: inline-block;">';
+
+// 开始替换
+// 只有当正文里真的包含“[惊喜]”时才执行替换，节省性能
 if (body.indexOf("[惊喜]") !== -1) {
-    body = body.replace(oldText, newImage);
+    // 替换文字为图片标签
+    body = body.replace(targetText, newImage);
+    // 结束并返回修改后的数据
     $done({ body });
 } else {
+    // 没找到就原样返回，不修改
     $done({});
 }
